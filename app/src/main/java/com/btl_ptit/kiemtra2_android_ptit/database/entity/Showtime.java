57d559f5f77329af22f5b35d5d60ea6c20bacffe
Minh <doanminhyb04@gmail.com>
2026-03-27 10:48:16 +0700

    add entites and DAO

---
+++ app/src/main/java/com/btl_ptit/kiemtra2_android_ptit/database/entity/Showtime.java
@@ -0,0 +1,22 @@
+﻿package com.btl_ptit.kiemtra2_android_ptit.database.entity;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDateTime;
+
+@Entity(tableName = "showtimes", foreignKeys = {
+    @ForeignKey(entity = Movie.class, parentColumns = "id", childColumns = "movieId", onDelete = ForeignKey.CASCADE),
+    @ForeignKey(entity = Theater.class, parentColumns = "id", childColumns = "theaterId", onDelete = ForeignKey.CASCADE)
+})
+public class Showtime {
+    @PrimaryKey(autoGenerate = true) public int id;
+    public int movieId;
+    public int theaterId;
+    public LocalDateTime startTime;
+
+    public LocalDateTime endTime;
+
+    public int seatNum;
+
+}
