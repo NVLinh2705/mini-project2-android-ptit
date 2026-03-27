eb148e2d3d47945bb2cb8a0a220f933c33b3b751
shormez <nguyenduclam478@gmail.com>
2026-03-27 10:54:54 +0700

    add HomeActivity, activity_home.xml, activity_movie.xml, MovieActivity

--- app/src/main/java/com/btl_ptit/kiemtra2_android_ptit/view/activity/HomeActivity.java
+++ app/src/main/java/com/btl_ptit/kiemtra2_android_ptit/view/activity/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.btl_ptit.kiemtra2_android_ptit.view.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +13,75 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.btl_ptit.kiemtra2_android_ptit.R;
+import com.btl_ptit.kiemtra2_android_ptit.databinding.ActivityHomeBinding;
+import com.btl_ptit.kiemtra2_android_ptit.utils.Constants;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+    private String username;
+
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        sp= getSharedPreferences(Constants.PREF_NAME, MODE_PRIVATE);
+        username= sp.getString(Constants.KEY_USERNAME, "");
+
+        if(!username.isEmpty()){
+            binding.helloUserTxt.setText("Xin chào "+ username);
+            binding.loginBtn.setText("Đăng xuất");
+            binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sp.edit().clear().apply();
+                    binding.helloUserTxt.setText("");
+                    binding.loginBtn.setText("Đăng nhập");
+                }
+            });
+        }else{
+            binding.helloUserTxt.setText("");
+            binding.loginBtn.setText("Đăng nhập");
+            binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+//                    startActivity(intent);
+                    Toast.makeText(HomeActivity.this,"click vao login",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        binding.movieBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MovieActivity.class);
+                startActivity(intent);
+                Toast.makeText(HomeActivity.this,"click vao movie",Toast.LENGTH_SHORT).show()
+            }
         });
+
+        binding.theaterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(HomeActivity.this, TheaterActivity.class);
+//                startActivity(intent);
+                 Toast.makeText(HomeActivity.this,"click vao theater",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.showtimeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(HomeActivity.this, ListShowtimeActivity.class);
+//                startActivity(intent);
+                Toast.makeText(HomeActivity.this,"click vao showtime",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/btl_ptit/kiemtra2_android_ptit/view/activity/MovieActivity.java
@@ -0,0 +1,21 @@
+package com.btl_ptit.kiemtra2_android_ptit.view.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.btl_ptit.kiemtra2_android_ptit.R;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+    }
+}
